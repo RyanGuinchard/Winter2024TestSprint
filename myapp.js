57733185ed0fd1f9793c690efc009aa1fe7a8dc8
Ryan Guinchard <ryanguinchard9@hotmail.com>
2024-01-31 12:58:39 -0330

    File

--- myapp.js
+++ myapp.js
@@ -5,3 +5,10 @@ const fs = require('fs');
 const myArgs = process.argv.slice(2);
 
 if(DEBUG) if (myArgs.length >= 1) console.log('the myapp.args: ', myArgs);
+
+switch(myArgs[0]) {
+    case 'init':
+    case 'i':
+        if(DEBUG) console.log(myArgs[0], ' - initiaze the app')
+        break;
+}
